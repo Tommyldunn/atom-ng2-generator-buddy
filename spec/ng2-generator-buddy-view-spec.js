08eb3b99160b1a3a2c2d805e4ebf38bc2b24271b
tommyldunn <tommylogandunn@gmail.com>
2017-03-03 05:59:38 -0500

    init

---
+++ spec/ng2-generator-buddy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Ng2GeneratorBuddyView from '../lib/ng2-generator-buddy-view';
+
+describe('Ng2GeneratorBuddyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
