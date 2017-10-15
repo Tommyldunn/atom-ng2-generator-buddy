406e6c0ae31661697ea50d4849388585fb3971e9
tommyldunn <tommylogandunn@gmail.com>
2017-10-15 00:14:36 -0400

    removed testing dir && added lock

--- lib/test/r/r.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'r',
-    templateUrl: 'r.component.html',
-    styleUrls: ['r.component.scss']
-})
-export class RComponent { }
